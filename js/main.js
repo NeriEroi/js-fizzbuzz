827ade09c8f77cae14377bd3b54ffce4b8eb4ae0
Michele <micheleierniero@gmail.com>
2021-07-19 15:46:47 +0200

    Stampa FizzBuzz per multipli di 3 e di 5

--- js/main.js
+++ js/main.js
@@ -2,31 +2,36 @@
 
 // Stampa Fizz per multipli di 3
 
-// Stampa Fizz per multipli di 5
+// Stampa Buzz per multipli di 5
+
+// Stampa FizzBuzz per multipli di 3 e di 5
 
 var numeroIterazioni = 100;
 
+console.log(i + 1);
 
 
 for (var i = 0; i < numeroIterazioni; i++)
 
 {
-    console.log(i + 1);
-    
-    if ((i % 3 == 0))
+    if (i % 3 == 0)
     {    
 
-        console.log(i + "Fizz");
+        console.log(i + " Fizz");
 
     }
-
-    else if ((i % 5 == 0))
+    else if (i % 5 == 0)
     {    
 
-        console.log(i + "Buzz");
+        console.log(i + " Buzz");
 
     }
+    if (i % 3 == 0 && i % 5 == 0)
+    {
 
+        console.log(i + " FizzBuzz");
+
+    }
 }
 
 
