35d7360a61d9a4e5629f060d8e909db9152edd88
Michele <micheleierniero@gmail.com>
2021-07-19 15:17:42 +0200

    stampa Fizz per multipli di 3

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Stampa numeri da 1 a 100
+
+// Stampa Fizz per multipli di 3
+
+var numeroIterazioni = 100;
+
+
+for (var i = 0; i < numeroIterazioni; i++)
+
+{
+    console.log(i + 1);
+
+    if ((i % 3 == 0))
+    { 
+
+    console.log("Fizz");
+
+    }
+}
+
+
+
+
+
+
+
+
+
